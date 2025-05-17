dae134fcd88e2cacb6e70c7709a6141605977ea9
Vladtsrez <Competitionvladinf@gmail.com>
2025-05-17 11:38:58 +0300

    port fix

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def send_email():
         return render_template('index.html', message=f"Error sending email: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
